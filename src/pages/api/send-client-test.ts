f7b806a5c9a9a945e24b9189f58e1461e49ba2e2
hossiiii <hossiiii0117@gmail.com>
2023-05-09 05:54:38 +0900

    テスト2

--- src/pages/api/send-client-test.ts
+++ src/pages/api/send-client-test.ts
@@ -7,109 +7,52 @@ import {
   Mosaic,
   MosaicId,
   RepositoryFactoryHttp,
+  TransactionService,
   TransferTransaction,
   UInt64,
 } from 'symbol-sdk';
 import { firstValueFrom } from 'rxjs';
-import axios from 'axios';
-import { WebSocket } from 'ws';
 import { connectNode } from '@/utils/connectNode';
 import { nodeList } from '@/consts/nodeList';
 
-type responseType = {
-  statusCode: number;
-  body: string;
-};
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
+  if (req.method === 'POST') {
     const NODE = await connectNode(nodeList);
     if (NODE === '') return '';
-    // 環境変数から呼び出し
-    const repo = new RepositoryFactoryHttp(NODE);
+    const repo = new RepositoryFactoryHttp(NODE, {
+      websocketUrl: NODE.replace('http', 'ws') + '/ws',
+      websocketInjected: WebSocket,
+    });
     const txRepo = repo.createTransactionRepository();
+    const receiptRepo = repo.createReceiptRepository();
+    const transactionService = new TransactionService(txRepo, receiptRepo);    
+
+    const listener = repo.createListener();
+
     const epochAdjustment = await firstValueFrom(repo.getEpochAdjustment());
     const generationHash = await firstValueFrom(repo.getGenerationHash());
     const networkType = await firstValueFrom(repo.getNetworkType());
-    const minFeeMultiplier = 100;
+    const currencies = await firstValueFrom(repo.getCurrencies());
 
-    const privatekey = process.env.PRIVATE_KEY!;
-    const admin = Account.createFromPrivateKey(privatekey, networkType);
-    const recipientAddress = Address.createFromRawAddress(req.body.address);
+    const admin = Account.createFromPrivateKey(process.env.PRIVATE_KEY!, networkType);
+    const clientAddress = Address.createFromRawAddress(req.body.address);
 
-    // 転送トランザクション作成
-    const transferTx = TransferTransaction.create(
+    const tx = TransferTransaction.create(
       Deadline.create(epochAdjustment),
-      recipientAddress,
-      [new Mosaic(new MosaicId('72C0212E67A08BCE'), UInt64.fromUint(1000000))],
+      clientAddress,
+      [new Mosaic(currencies.currency.mosaicId as MosaicId, UInt64.fromUint(1000000))],
       EmptyMessage,
       networkType
-    ).setMaxFee(minFeeMultiplier);
+    ).setMaxFee(100);
 
-    // 署名およびアナウンス
-    if (generationHash == undefined) throw new Error('generationHash not found.');
-    const signedTx = admin.sign(transferTx, generationHash);
+    const signedTx = admin.sign(tx, generationHash);
     await firstValueFrom(txRepo.announce(signedTx));
-
-    // アナウンスしたトランザクションの監視
-    const transactionStatusUrl = NODE + '/transactionStatus/' + signedTx.hash;
-
-    const response = await axios.get(transactionStatusUrl)
-    if (response.data.code == 'Success') {
-      return res.status(200).json(signedTx.hash);
-    }
-    const wsEndpoint = NODE.replace('http', 'ws') + '/ws';
-    const ws = new WebSocket(wsEndpoint);
-    ws.onopen = () => console.log('start monitoring transaction');
-
-    const res2 = (): Promise<string> => {
-      return new Promise((resolve) => {
-        ///監視をスタートして10秒間トランザクションを検知できなかった時の処理
-        const timerId = setTimeout(async function () {
-          try {
-            const response = await axios.get(transactionStatusUrl);
-            ///アナウンスと同時に承認されタイミング悪く監視上は検知できなかった場合の処理
-            if (response.data.code == 'Success') {
-              resolve(signedTx.hash);
-              ///残高不足やアドレスが見つからないなどで通知はされたが承認されなかった場合の処理
-            } else {
-              resolve('');
-            }
-            ///手数料不足などでアナウンスされたトランザクションがノードに認識されていない状態
-          } catch {
-            resolve('');
-          }
-        }, 1000);
-
-        // 監視をスタートして5秒以内にトランザクションを検知できた時の処理
-        ws.onmessage = (e) => {
-          const response = JSON.parse(e.data.toString());
-          if ('uid' in response) {
-            const body = '{"uid":"' + response.uid + '", "subscribe":"block"}';
-            const transaction =
-              '{"uid":"' +
-              response.uid +
-              '","subscribe":"unconfirmedAdded/' +
-              recipientAddress.plain() +
-              '"}';
-            ws.send(body);
-            ws.send(transaction);
-          }
-          if (response.topic == 'unconfirmedAdded/' + recipientAddress.plain()) {
-            console.log('close');
-            ws.close();
-            clearTimeout(timerId);
-            resolve(signedTx.hash);
-          }
-        };
-      });
-    };
-
-    const r = await res2();
-    console.log(r)
-    return res.status(200).json(r);
-  } catch (e) {
-    console.error(e);
-    return '';
+    await listener.open();
+    listener.unconfirmedAdded(clientAddress).subscribe((unconfirmedTx) => {
+      console.log(unconfirmedTx);
+      if (unconfirmedTx.transactionInfo?.hash === signedTx.hash) {
+        res.status(200).json(signedTx.hash);
+      }
+    });
   }
-};
+}
